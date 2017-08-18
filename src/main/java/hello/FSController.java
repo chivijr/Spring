10d4c240cf49c7a094af9d049e19b3f91753bd1f
Chivijr <chivijr@gmail.com>
2017-08-18 02:01:35 +0200

    Primera versión FS funcional.
    Reformatea texto.

--- src/main/java/hello/FSController.java
+++ src/main/java/hello/FSController.java
@@ -1,11 +1,14 @@
 package hello;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FSController {
 
-//    @RequestMapping(value = "/fs",
+    //    @RequestMapping(value = "/fs",
     //        method = RequestMethod.GET,
     //        produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
     //        consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
@@ -13,7 +16,9 @@ public class FSController {
     @CrossOrigin(origins = "http://localhost:8081")
     @RequestMapping("/fs")
     public FS fs(@RequestParam(value = "dir", defaultValue = "/") String dir) {
-        if (dir.equals("null")) {dir = "/";}
+        if (dir.equals("null")) {
+            dir = "/";
+        }
         return new FS(dir);
     }
 }
